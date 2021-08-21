47a49106e6680613e04a1526abeb450a02bb8c40
yangpt <yangpt@mail.com>
2021-08-22 00:16:09 +0800

    'AddTwoNumbers'

---
+++ 002_Add_Two_Numbers/src/com/xya/leetcode/ListNode.java
@@ -0,0 +1,21 @@
+package com.xya.leetcode;
+
+/**
+ * Created by xujian on 2021-08-21.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
