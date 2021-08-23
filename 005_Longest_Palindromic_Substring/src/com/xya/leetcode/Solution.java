e404cd15232f6be44c62b3e4f0ab78da2705cdc9
xujian <527295205@qq.com>
2021-08-23 11:32:36 +0800

    Palindrome_Number

---
+++ 005_Longest_Palindromic_Substring/src/com/xya/leetcode/Solution.java
@@ -0,0 +1,28 @@
+package com.xya.leetcode;
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * 示例 1：
+ *
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * 示例 3：
+ *
+ * 输入：s = "a"
+ * 输出："a"
+ * 示例 4：
+ *
+ * 输入：s = "ac"
+ * 输出："a"
+ */
+public class Solution {
+    public String longestPalindrome(String s) {
+        String result = "";
+        return result;
+    }
+}
